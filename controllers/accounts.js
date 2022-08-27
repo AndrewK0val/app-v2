b664f9d3c5207dfc78072cc676ba6bf1314e9b8c
Glitch (leaf-screeching-poison) <none>
2022-08-27 16:47:20 +0000

    Import of WebAppDev1/lab10-complete

---
+++ controllers/accounts.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const userstore = require('../models/user-store');
+const logger = require('../utils/logger');
+const uuid = require('uuid');
+
+const accounts = {
+
+  index(request, response) {
+    const viewData = {
+      title: 'Login or Signup',
+    };
+    response.render('index', viewData);
+  },
+
+  login(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('login', viewData);
+  },
+
+  logout(request, response) {
+    response.cookie('playlist', '');
+    response.redirect('/');
+  },
+
+  signup(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('signup', viewData);
+  },
+
+  register(request, response) {
+    const user = request.body;
+    user.id = uuid();
+    user.picture=request.files.picture,
+    userstore.addUser(user, function() {
+       logger.info('registering' + user.email);
+       response.cookie('playlist', user.email);
+       logger.info('logging in' + user.email);
+       response.redirect('/start');
+    });
+  },
+
+  authenticate(request, response) {
+    const user = userstore.getUserByEmail(request.body.email);
+    if ((user) && (user.password === request.body.password)) {
+      response.cookie('playlist', user.email);
+      logger.info('logging in' + user.email);
+      response.redirect('/start');
+    } else {
+      response.redirect('/login');
+    }
+  },
+
+  getCurrentUser (request) {
+    const userEmail = request.cookies.playlist;
+    return userstore.getUserByEmail(userEmail);
+  }
+}
+
+module.exports = accounts;
